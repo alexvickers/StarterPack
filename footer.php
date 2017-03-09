17c7c37f6f9485216aa233b05efbb6a102956479
alexvickers <alexvickers@gmail.com>
2017-03-09 16:42:21 -0300

    Arquivos base em php.

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+</footer>
+<!--jQuery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<!--Bootstrap-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!--bxSlider4 Rahisified-->
+<script src="js/jquery.bxslider-rahisified.min.js"></script>
+<!--Plugins-->
+<script src="js/plugins.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title></title>
+	
+    <!--Favicon-->
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico">
+    <!--Botstrap-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!--Estilos-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/mediaqueries.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <!--Fonte-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+</head>
+<body>
+<header class="container-fluid">
+</header>
