48a87f6df2dbadea19be84665df6fa59cc182a65
Alexandre Aimbiré <alexvickers@gmail.com>
2017-05-05 10:28:48 -0300

    Add banner fixes to mobile.

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php include 'header.php';?>
 <main>
+    <section class="banner">
+        <ul class="bxslider" data-call="bxslider" data-options="autoReload:true, oneToOneTouch:false">
+        </ul>
+    </section>
 </main>
 <?php include 'footer.php';?>
